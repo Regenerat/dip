24fa8642a639b3706227202537aa5e7d783c5e5d
Megira <regenerat.55@mail.ru>
2024-04-15 11:56:06 +0300

    Добавил блок с новинками

--- views/site/index.php
+++ views/site/index.php
@@ -1,10 +1,17 @@
 <?php
 
+use app\models\Status;
 use yii\bootstrap5\Html;
 
 /** @var yii\web\View $this */
 
 $this->title = 'My Yii Application';
+
+$newBooks = \app\models\Books::find()
+    ->where(['status_id' => Status::NEW_STATUS])
+    ->all();
+
+$this->registerJsFile('https://code.jquery.com/jquery-3.6.0.min.js', ['position' => \yii\web\View::POS_HEAD]);
 ?>
 <div class="site-index">
     <div class="container">
@@ -43,4 +50,31 @@ $this->title = 'My Yii Application';
             </div>
         </div>
     </div>
+    <div class="new__books">
+        <div class="container">
+            <p class="new__books__title">Новинки</p>
+            <div class="row">
+                <?php foreach ($newBooks as $book): ?>
+                    <div class="book_item">
+                        <div class="thumbnail">
+                            <?= Html::img('../../' . $book->image, ['alt' => $book->title, 'class' => 'img-responsive']) ?>
+                            <div class="caption">
+                                <p class="book__title"><?= Html::encode($book->title) ?></p>
+                                <p class="book__author"><?= Html::encode($book->author->mName. ' ' . mb_substr($book->author->fName, 0, 1). '.' . mb_substr($book->author->lName, 0, 1).'.') ?></p>
+                                <div class="row buy__block">
+                                    <div class="col-6">
+                                        <p class="book__price"><?= Html::encode($book->price)."₽" ?></p>
+                                    </div>
+                                    <div class="col-4">
+                                        <?= Html::a('Купить', ['purchase', 'id' => $book->id], ['class' => 'btn buy__btn']) ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div> 
 </div>
+
