b6fea08ae2a62855593d8c5728ae90eaebf0cc13
Megira <regenerat.55@mail.ru>
2024-05-21 10:50:27 +0300

    сгенерировал вьюшки для корзины

---
+++ views/cart/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use app\models\Cart;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var app\models\CartSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Carts');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="cart-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Cart'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'user_id',
+            'book_id',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Cart $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
