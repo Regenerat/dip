94d73a7b3f5b4716d84fe620ff4de9d4ac6e57af
Megira <regenerat.55@mail.ru>
2024-05-21 10:49:14 +0300

    сменил БД в конфиге

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:1403;dbname=dip',
+    'dsn' => 'mysql:host=127.0.0.1:1403;dbname=dip2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
