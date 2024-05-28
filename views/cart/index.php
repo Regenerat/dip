f9f63e52c577ea0979bcd485d028c359946d9ac4
Megira <regenerat.55@mail.ru>
2024-05-28 17:07:34 +0300

    переписал главную

--- views/cart/index.php
+++ views/cart/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\models\Cart;
+use app\models\Books;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
@@ -10,36 +11,45 @@ use yii\grid\GridView;
 /** @var app\models\CartSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = Yii::t('app', 'Carts');
-$this->params['breadcrumbs'][] = $this->title;
+$newBooks = Cart::find()
+    ->where(['user_id' => Yii::$app->user->identity->id])
+    ->all();
 ?>
+
 <div class="cart-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Cart'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'user_id',
-            'book_id',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Cart $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="cart__order">
+                    <?php foreach ($newBooks as $book): ?>
+                        <div class="cart">
+                            <div class="cart__image">
+                                <?= Html::a(Html::img('../../' . $book->book->image, [
+                                    'alt' => $book->book->title, 
+                                    'class' => 'img-responsive',
+                                    'style' => 'cursor: pointer;'
+                                ]), ['books/view', 'id' => $book->book->id]) ?>
+                            </div>
+                            <div class="cart__info">
+                                <p class="cart__title"><?= Html::encode($book->book->title) ?></p>
+                                <p class="cart__author"><?= Html::encode($book->book->author->fName) ?><?= Html::encode($book->book->author->mName) ?></p>
+                            </div>
+                            <div class="cart__price">
+                                <?= Html::encode($book->book->price) ?><small>₽</small>
+                            </div>
+                            
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="cart__order__info">
+                    <p class="cart__count"><?= Html::encode(count($newBooks)) ?> товара</p>
+                </div>
+            </div>
+        </div>
+        
+        
+    </div>
 
 </div>
