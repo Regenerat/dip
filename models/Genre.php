1c3151d6382994f6ec59ed4cf2def69ecb2315e0
Megira <regenerat.55@mail.ru>
2024-05-28 17:06:05 +0300

    сгенерировал модели

---
+++ models/Genre.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "genre".
+ *
+ * @property int $id
+ * @property string $genre
+ *
+ * @property Books[] $books
+ */
+class Genre extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'genre';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['genre'], 'required'],
+            [['genre'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'genre' => 'Genre',
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Books::class, ['genre_id' => 'id']);
+    }
+}
---
+++ models/Orders.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "orders".
+ *
+ * @property int $id
+ * @property string $date
+ * @property int $user_id
+ * @property int $book_id
+ * @property int $quantity
+ *
+ * @property Books $book
+ * @property User $user
+ */
+class Orders extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'orders';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['date', 'user_id', 'book_id', 'quantity'], 'required'],
+            [['date'], 'safe'],
+            [['user_id', 'book_id', 'quantity'], 'integer'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+            [['book_id'], 'exist', 'skipOnError' => true, 'targetClass' => Books::class, 'targetAttribute' => ['book_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'date' => 'Date',
+            'user_id' => 'User ID',
+            'book_id' => 'Book ID',
+            'quantity' => 'Quantity',
+        ];
+    }
+
+    /**
+     * Gets query for [[Book]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBook()
+    {
+        return $this->hasOne(Books::class, ['id' => 'book_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
