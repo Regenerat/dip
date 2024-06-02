87a2ff1f6d4dbeeb2dbb5d9bfc25cd1d93bfa172
Megira <regenerat.55@mail.ru>
2024-06-02 17:38:35 +0300

    another fixes

--- views/site/index.php
+++ views/site/index.php
@@ -54,15 +54,14 @@ $newBooks = \app\models\Books::find()
 
 <div class="site-index">
     <div class="container">
-        <div class="row">
-            <div class="col-9">
+
                 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/home_slider/slide1.png" alt="1" class="d-block w-100">
                         </div>
                         <div class="carousel-item">
-                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/home_slider/slide2.png" alt="2" class="d-block w-100">
+                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/home_slider/slide2.jpg" alt="2" class="d-block w-100">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -74,20 +73,7 @@ $newBooks = \app\models\Books::find()
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
-            </div>
-            <div class="col-3">
-                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/home_slider/slide3.png" alt="1" class="d-block w-100">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="<?= Yii::$app->urlManager->baseUrl ?>/images/home_slider/slide3.png" alt="2" class="d-block w-100">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     </div>
     <div class="new__books">
         <div class="container">
